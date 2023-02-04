dcd37f794426b7dca649694e77c5e8700be70e8d
Moisés <moisesleyva@outlook.com>
2023-02-05 00:16:53 +0100

    Trigger PickedUp event when the animation has finished.

--- ggj2023Project/Assets/PickingUpFinished.cs
+++ ggj2023Project/Assets/PickingUpFinished.cs
@@ -6,9 +6,8 @@ public class PickingUp : StateMachineBehaviour
 	public static event PickedUp OnPickedUp;
 
 	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-		Debug.Log("Picked up");
 		// Set animator property
         animator.SetBool("isPickingUp", false);
 
