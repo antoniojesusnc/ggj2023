f7c8a7acfaee008fa708210dcc8846123a0f3fb1
AntonioZu <antoniojesusnc@gmail.com>
2023-02-05 00:30:47 +0100

    audio manager

--- ggj2023Project/Assets/Scripts/Character/CharacterManager.cs
+++ ggj2023Project/Assets/Scripts/Character/CharacterManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Character;
 using UnityEngine;
 
@@ -6,8 +7,20 @@ public class CharacterManager : MonoBehaviour
     [field: SerializeField] 
     public Transform HeadTransform { get; private set; }
 
+    private void Start()
+    {
+        GameManager.Instance.OnShakeStatusChanged += OnShakeStatusChanged;
+    }
+
+    private void OnShakeStatusChanged(bool shaking)
+    {
+        GetComponent<AudioListener>().enabled = shaking;
+    }
+
     public void InTrigger(ItemDetector itemDetector)
     {
         GetComponent<CharacterItemInteraction>().SetTrigger(itemDetector);
     }
+    
+    
 }
